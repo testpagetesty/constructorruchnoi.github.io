d1ec2ce93d932b75026e9d4a49e0ecc033032cbb
constructorruchnoi <apartik0101@gmail.com>
2025-08-17 20:31:03 +0300

    Update constructor components and remove duplicate folder

--- Новая папка (2)/src/utils/formLanguageDetector.js
+++
@@ -1,7 +0,0 @@
-// Пустая функция без языкового определения
-export const generateFormLanguageScript = () => {
-  return `
-                // Пустой скрипт - языковая логика удалена
-                console.log('Form script loaded - no language detection');
-  `;
-}; 
